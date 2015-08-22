d87c3b65676b4974f2bddb10dc72ad7876c0a6c6
shekhar270779 <p.shekhar.2707@gmail.com>
2015-08-23 03:54:48 +0530

    Java tut1 HelloWorld

---
+++ Tutorial1/src/Hello.java
@@ -0,0 +1,43 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//First program to say Hello to the world
+		
+		System.out.println("Hello World!!");
+		
+		//variables
+		
+		int myNumber; //32bit
+		myNumber = 79;
+		System.out.println(myNumber);
+		
+		short myShort;//16 bit
+		myShort=23;
+		System.out.println(myShort);
+		
+		long myLong; //64bit
+		myLong=770901036;
+		System.out.println(myLong);
+		
+		double myDouble=3.14732; //64 bit
+		System.out.println(myDouble);
+		
+		float myFloat; //32 bit
+		myFloat=4.732f;
+		System.out.println(myFloat);
+		
+		char myChar; //16 bit unicode char
+		myChar='Y';
+		System.out.println(myChar);
+		
+		boolean flag;
+		flag=true;
+		System.out.println(flag);
+		
+		byte myByte;//8 bit
+		myByte=127;
+		System.out.println(myByte);
+	}
+
+}
