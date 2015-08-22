ea97b2b72c1f333ed296b511b1f70abdda42c560
shekhar270779 <p.shekhar.2707@gmail.com>
2015-08-23 04:09:19 +0530

    change 1

--- Tutorial1/src/Hello.java
+++ Tutorial1/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		//First program to say Hello to the world
 		
-		System.out.println("Hello World!!");
+		System.out.println("Hello Beautiful World!!");
 		
 		//variables
 		
